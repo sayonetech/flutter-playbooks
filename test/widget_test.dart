0c3190cb6d9200c1751399835b94d4f5d145e255
initiuminc1 <renjith@renjith-mac.local>
2018-03-27 16:46:25 +0530

    Test case updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:helloworld/app/pages/home.dart';
+import 'package:helloworld/app/views/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
