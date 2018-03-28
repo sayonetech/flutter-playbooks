6afec99577318854300b4a25699d0a1da8cedf1d
Renjith <renjith@sayonetech.com>
2018-03-28 14:46:29 +0530

    Implemented Store Model

---
+++ lib/app/models/store.dart
@@ -0,0 +1,12 @@
+class Store {
+  Store({
+    this.name,
+    this.id,
+    this.mallId,
+    this.type
+  });
+  final String name;
+  final String id;
+  final String mallId;
+  final String type;
+}
