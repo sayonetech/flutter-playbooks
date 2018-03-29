94298d2bd9bd66f43a938cef4ff9d07ab9b082e6
Renjith <renjith@sayonetech.com>
2018-03-29 17:49:05 +0530

    Splash screen

--- lib/app/config/injector.dart
+++ lib/app/config/injector.dart
@@ -1,5 +1,4 @@
 import '../network/api.dart';
-import 'package:event_bus/event_bus_hierarchical.dart';
 
 
 
@@ -7,11 +6,9 @@ class Injector {
 
   static final Injector injector = new Injector._injector();
   ApiClient _apiClient;
-  EventBus _eventBus;
 
   Injector._injector(){
     _apiClient = new ApiClient();
-    _eventBus = new HierarchicalEventBus();
   }
 
 
@@ -20,6 +17,4 @@ class Injector {
   }
 
   ApiClient get apiClient => _apiClient;
-
-  EventBus get eventBus => _eventBus;
 }
