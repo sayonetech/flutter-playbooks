ed9ff8beb218c08f93816dfddaa6b7d628526e3b
Renjith <renjith@sayonetech.com>
2018-04-07 16:29:01 +0530

    Await and async in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:helloworld/app/app_component.dart';
 import 'package:helloworld/app/config/injector.dart';
 import 'package:helloworld/app/events/store_event.dart';
 import 'package:flutter/services.dart';
+import 'package:helloworld/app/models/store.dart';
 
 void main() async {
   Injector _injector = new Injector();
@@ -13,10 +14,16 @@ void main() async {
   ClipboardData data = await Clipboard.getData(Clipboard.kTextPlain);
   print(data.text);
 
+  //OLD Implemetation
+  /*
   _injector.apiClient.fetchMalls("12.969840","77.582443").then((stores) {
     print(stores);
     _injector.eventBus.fire(new StoreLoadedEvent(stores));
-  });
+  });*/
+
+  //New & Simple
+  List<Store> stores = await _injector.apiClient.fetchMalls("12.969840","77.582443");
+  _injector.eventBus.fire(new StoreLoadedEvent(stores));
 
   _injector.eventBus.on().listen((event) {
     // Print the runtime type. Such a set up could be used for logging.
